cee36e40e919a8491ced10d5a14000b624bb8df1
Michael Whipps <michaelwhipps@MacBook-Pro.local>
2025-02-19 17:06:55 +0900

    feat: add useState for config and useEffect for setting config. Also add handler function for setting config.

--- src/components/Synth1.tsx
+++ src/components/Synth1.tsx
@@ -1,10 +1,40 @@
 import * as Tone from "tone";
 import { Piano, KeyboardShortcuts } from "react-piano";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "react-piano/build/styles.css";
 
 const Synth1: React.FC = () => {
   const [synth] = useState(() => new Tone.Synth().toDestination());
+  console.log(synth.get());
+
+  const [config, setConfig] = useState({
+    frequency: "C1",
+    detune: 0,
+    oscillatorType: "square",
+    envelopeAttack: 0.005,
+    envelopeDecay: 0.1,
+    envelopeSustain: 0.9,
+    envelopeRelease: 1,
+  });
+
+  useEffect(() => {
+    synth.frequency.value = config.frequency;
+    synth.detune.value = config.detune;
+    synth.envelope.attack = config.envelopeAttack;
+    synth.envelope.decay = config.envelopeDecay;
+    synth.envelope.sustain = config.envelopeSustain;
+    synth.envelope.release = config.envelopeRelease;
+  }, [config, synth]);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      [name]: value,
+    }));
+  };
 
   const firstNote = 24; // MIDI number for C1
   const lastNote = 41; // MIDI number for F2
@@ -27,6 +57,7 @@ const Synth1: React.FC = () => {
   return (
     <div>
       <h1>Synth1</h1>
+
       <Piano
         noteRange={{ first: firstNote, last: lastNote }}
         onPlayNote={playNote}
